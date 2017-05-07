7ad70f9d3e766662c358e7a3ff89e51dd8599f7f
oem <oem@LeeHanna>
2017-05-07 21:27:02 +0900

    대시보드 ... 휴...

--- studyroom/src/main/java/com/monorella/srf/branch/dashboard/DashboardController.java
+++ studyroom/src/main/java/com/monorella/srf/branch/dashboard/DashboardController.java
@@ -6,13 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.monorella.srf.branch.member.MemberDao;
-
 @Controller
 public class DashboardController {
 	
 	@Autowired
-	MemberDao memberDao;
+	DashboardDao dashboardDao;
 	
 	// 차트 
 	@RequestMapping(value="/chart" , method = RequestMethod.GET)
@@ -20,17 +18,22 @@ public class DashboardController {
 		System.out.println("DashboardController-> chart()");
 		return "dashboard/chart";
 	}
+	
 	// 메뉴 - 회원 현황 폼
 	@RequestMapping(value="/dashboard/member_status" , method = RequestMethod.GET)
-	public String selectMemberStatus(){
+	public String selectMemberStatus(Model model){
 		System.out.println("DashboardController-> selectMemberStatus()");
+		int useMemberNum = dashboardDao.selectUseMember();
+		model.addAttribute("useMemberNum", useMemberNum);
 		return "dashboard/member_status";
 	}
 	// 오늘의 현황 폼 요청
 	@RequestMapping(value="/dashboard/home" , method = RequestMethod.GET)
 	public String home(Model model){
 		System.out.println("DashboardController-> home()");
-		int memberNom = memberDao.selectMemberCount();
+		int memberNom = dashboardDao.selectMemberCount();
+		int useMemberNum = dashboardDao.selectUseMember();
+		model.addAttribute("useMemberNum", useMemberNum);
 		System.out.println("DashboardController-> home()-> membeNom: "+memberNom);
 		model.addAttribute("memberNom", memberNom);
 		return "dashboard/home";
